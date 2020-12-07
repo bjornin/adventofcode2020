d5c52201a77e62fa66f2a48d0c4edbf3c4e8e072
Björn Ingeson <bjorn.ingeson@gmail.com>
2020-12-07 12:18:17 +0000

    day6 part1

---
+++ day7/day7.py
@@ -0,0 +1,42 @@
+import re
+
+def part1(arg):
+    parent = set()
+    queue = ['shiny gold']
+    while len(queue) > 0:
+        color = queue.pop()
+        for k, v in arg.items():
+            if color in [c[1] for c in v]:
+                parent.add(k)
+                queue.insert(0,k)
+    return len(parent)
+
+def part2(arg):
+    pass
+
+def get_src(s):
+    src_pattern = r'^(?P<src_color>\w+\s\w+)'
+    m = re.search(src_pattern, s)
+    return m['src_color']
+
+def get_dst(s):
+    dst_pattern = r'(?P<number>\d+)\s(?P<dst_color>\w+\s\w+)'
+    m = re.findall(dst_pattern, s)
+    return m
+
+def clean_input(input):
+    a = {}
+    for i in input:
+        src = get_src(i)
+        groups = get_dst(i)
+        a[src] = a.get(src, [])
+        a[src].extend(groups)
+    return a
+
+input = []
+with open('./input.txt', 'r') as f:
+    input = [x.rstrip() for x in f]
+
+print('input rows:', len(input))
+print('part1', part1(clean_input(input)))
+print('part2', part2(input))
