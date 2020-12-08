3d77fe220d3d929bfc40c0b478686ce76be18e44
Björn Ingeson <bjorn.ingeson@gmail.com>
2020-12-08 07:01:41 +0000

    day8 part2

--- day8/day8.py
+++ day8/day8.py
@@ -15,7 +15,29 @@ def part1(arg):
     return acc
 
 def part2(arg):
-    pass
+    for line in range(len(arg)):
+        if arg[line].startswith('acc'):
+            continue
+        swapped = arg.copy()
+        if arg[line].startswith('nop'):
+            swapped[line] = swapped[line].replace('nop', 'jmp')
+        elif arg[line].startswith('jmp'):
+            swapped[line] = swapped[line].replace('jmp', 'nop')
+        i = 0
+        acc = 0
+        visited = [False] * len(swapped)
+        while not visited[i]:
+            visited[i] = True
+            inst, number = swapped[i].split(' ')
+            if inst == 'acc':
+                acc += int(number)
+                i += 1
+            elif inst == 'jmp':
+                i += int(number)
+            elif inst == 'nop':
+                i += 1
+            if (i == len(swapped)):
+                return acc
 
 input = []
 with open('./input.txt', 'r') as f:
