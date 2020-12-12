d093d3bf1dad44404283e9fc67c1e29f184cd302
Björn Ingeson <bjorn.ingeson@gmail.com>
2020-12-12 08:11:09 +0000

    day2 entire diag =P

--- day11/day11.py
+++ day11/day11.py
@@ -33,8 +33,65 @@ def part1(arg):
                 layout[seat] = update_seat(org_layout, seat, length)
     return layout.count('#')
 
+def get_horizontal(layout, seat, length):
+    start = seat // length * length
+    end = start + length
+    return range(start,end)
+
+def get_vertical(layout, seat, length):
+    start = get_col(seat, length)
+    return [i * length + start for i in range(len(layout)//length)]
+
+def get_rdiag(layout, seat, length):
+    col = get_col(seat, length)
+    row = get_row(seat, length)
+    start = (col - row) if 0 <= col - row < length else row * length + col
+    diag = [i * (length + 1) + start for i in range(length - col - row)]
+    return diag
+
+def get_ldiag(layout, seat, length):
+    col = get_col(seat, length)
+    row = get_row(seat, length)
+    start = (col + row) if col + row < length - 1 else row * col + row + col
+    diag = [i * (length - 1) + start for i in range(get_col(start, length) - get_row(start, length) + 1)]
+    return diag
+
+def get_col(seat, length):
+    return seat % length
+
+def get_row(seat, length):
+    return seat // length
+
+def get_neighbors_2(layout, seat, length):
+    mask = []
+    mask.extend(get_horizontal(layout, seat, length))
+    mask.extend(get_vertical(layout, seat, length))
+    mask.extend(get_rdiag(layout, seat, length))
+    mask.extend(get_ldiag(layout, seat, length))
+    ret = [layout[x] for x in set(mask)]
+    return ret
+
+def update_seat_2(layout, seat, length):
+    neighbors = get_neighbors_2(layout, seat, length)
+    if layout[seat] == 'L' and neighbors.count('#') == 0:
+        return '#'
+    elif layout[seat] == '#' and neighbors.count('#') > 5:
+        return 'L'
+    return layout[seat]
+
 def part2(arg):
-    pass
+    layout, length = arg
+    print(layout, length)
+    org_layout = []
+    round = 0
+    while org_layout != layout:
+        org_layout = layout.copy()
+        for seat in range(len(org_layout)):
+            if org_layout[seat] != '.':
+                layout[seat] = update_seat_2(org_layout, seat, length)
+        round += 1
+        print(round)
+    return layout.count('#')
 
 input = []
 with open('./input.txt', 'r') as f:
@@ -58,6 +115,6 @@ def clean_input(input):
     return (list(''.join(input)), length)
 
 print('input rows:', len(input))
-print('part1', part1(clean_input(input)))
+# print('part1', part1(clean_input(input)))
 # print('part1', part1(clean_input(test)))
-print('part2', part2(input))
+print('part2', part2(clean_input(test)))
