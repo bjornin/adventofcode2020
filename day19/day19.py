edbbb9c49ee60e9487e1d03bf8f56e18e2d72054
Björn Ingeson <bjorn.ingeson@gmail.com>
2020-12-22 23:16:30 +0000

    day19 part2

--- day19/day19.py
+++ day19/day19.py
@@ -1,61 +1,48 @@
 def part1(arg):
     rules, messages = parse_input(arg)
     count = 0
-    for m in messages:
-        if m in rules:
+    for message in messages:
+        if match_rules(message, rules['0'][0], rules):
             count += 1
     return count
 
 def part2(arg):
-    for i, a in enumerate(arg):
-        if a.split(':')[0] == '8':
-            arg[i] = '8: 42 | 42 8'
-        elif a.split(':')[0] == '11':
-             arg[i] = '11: 42 31 | 42 11 31'
     rules, messages = parse_input(arg)
+    rules['8'] = [['42'], ['42', '8']]
+    rules['11'] = [['42', '31'], ['42', '11', '31']]
     count = 0
-    for m in messages:
-        if m in rules:
+    for message in messages:
+        if match_rules(message, rules['0'][0], rules):
             count += 1
-            print(m)
     return count
+
+def match_rules(message, rule_list, rules):
+    if not rule_list:
+        return len(message) == 0
+    p = rule_list[0]
+    if p.isdigit():
+        for part in rules[p]:
+            if match_rules(message, part + rule_list[1:], rules):
+                return True
+    elif message.startswith(p):
+        return match_rules(message[1:], rule_list[1:], rules)
+    return False
 
 def parse_input(input):
     rules = {}
     messages = []
-    max_msg_size = 0
     for i in input:
         if len(i) == 0:
             continue
         elif i[0].isdigit():
             rule_nr, rule_pattern = i.split(': ')
-            rules.setdefault(int(rule_nr), rule_pattern.replace('"',''))
+            rule = []
+            for r in rule_pattern.split('|'):
+                rule.append([a.replace('"','') for a in r.split()])
+            rules[rule_nr] = rule
         else:
             messages.append(i)
-            max_msg_size = max(len(i), max_msg_size)
-    patterns = parse_rules(0, rules, max_msg_size)
-    return patterns, messages
-
-def parse_rules(index: int, rules, max_msg_size):
-    patterns = []
-    for rule in rules[index].split('|'):
-        part = []
-        for r in rule.strip().split(' '):
-            if r.isalpha():
-                p = r
-            elif index == int(r): # parse and filter out messages on the go instead!!!!
-                if index == 8:
-                    for _ in range(3):
-                        part = [x + y for x in part for y in p]
-                    break
-            else:
-                p = parse_rules(int(r), rules, max_msg_size)
-            if len(part):
-                part = ([x + y for x in part for y in p])
-            else:
-                part = p
-        patterns.extend(part)
-    return patterns
+    return rules, messages
 
 input = []
 with open('./input.txt', 'r') as f:
@@ -128,7 +115,7 @@ aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba
 
 
 print('input rows:', len(input))
-# print('part1', part1(input))
+print('part1', part1(input))
 # print('part1', part1(test.strip().split('\n')))
-# print('part2', part2(input))
-print('part2', part2(test2.strip().split('\n')))
+print('part2', part2(input))
+# print('part2', part2(test2.strip().split('\n')))
