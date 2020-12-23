cacffa3fcbb18d023dc2b9da92a454d08a9a1c9f
Björn Ingeson <bjorn.ingeson@gmail.com>
2020-12-23 22:31:13 +0000

    day23 part2

--- day23/day23.py
+++ day23/day23.py
@@ -1,7 +1,8 @@
 from collections import deque
+from array import array
 
 def part1(arg):
-    circle = deque([int(i) for i in arg], maxlen=len(arg))
+    circle = create_circle(arg, len(input))
     current_cup = circle[0]
     for _ in range(100):
         yanked = yank_after(current_cup, 3, circle)
@@ -9,9 +10,33 @@ def part1(arg):
         put_after(dest_cup, yanked, circle)
         next_current_cup_index = (circle.index(current_cup) + 1) % len(circle)
         current_cup = circle[next_current_cup_index]
-    return get_answer(circle)
+    return get_answer_1(circle)
 
-def get_answer(circle):
+def part2(arg):
+    circle = create_circle(arg, 1000000)
+    current_cup = circle[0]
+    for turn in range(10000000):
+        if turn % 100000 == 0:
+            print(turn)
+        yanked = yank_after(current_cup, 3, circle)
+        dest_cup = get_destination_cup(current_cup, circle)
+        put_after(dest_cup, yanked, circle)
+        next_current_cup_index = (circle.index(current_cup) + 1) % len(circle)
+        current_cup = circle[next_current_cup_index]
+    return get_answer_2(circle)
+
+def create_circle(input, maxlen):
+    cup_list = [int(i) for i in input] + list(range(len(input))
+    # cup_list = [int(i) for i in input] + list(range(len(input) + 1,maxlen + 1))
+    return cup_list
+
+def get_answer_2(circle):
+    ind = circle.index(1)
+    if ind == len(circle) - 1:
+        ind = -1
+    return circle[ind + 1] * circle[ind + 2]
+
+def get_answer_1(circle):
     start = circle.index(1)
     circle.rotate(-start)
     ans = ''
@@ -46,13 +71,10 @@ def get_destination_cup(current_cup, circle):
         except ValueError:
             target -= 1
 
-def part2(arg):
-    pass
-
 input = '712643589'
 test_input = '389125467'
 
-print('part1', part1(input))
+# print('part1', part1(input))
 # print('part1', part1(test_input))
-# print('part2', part2(get_input('./input.txt')))
-# print('part2', part2(get_input('./test_input.txt')))
+# print('part2', part2(input))
+print('part2', part2(test_input))
